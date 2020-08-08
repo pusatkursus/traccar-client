3c22442d2e0fd50be9c65162ca9e71333ed055a3
Pusat Kursus Komputer <69322822+pusatkursus@users.noreply.github.com>
2020-08-09 04:07:39 +0700

    Update post-provider.ts

--- src/providers/post-provider.ts
+++ src/providers/post-provider.ts
@@ -6,10 +6,6 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class PostProvider {
   server: string = 'http://192.168.43.45/traccar/';
-  //server: string = 'http://dirapratama.jasawebaplikasi.com/api/';
-  serverpulsa: string = 'https://kursuskomputer.web.id/pulsa/modul/';
-  serversosmed: string = 'https://kursuskomputer.web.id/pulsa/';
-  servertracar: string = 'http://192.168.43.45:5055/';
   constructor(
     public http : Http
     ) 
@@ -30,35 +26,6 @@ export class PostProvider {
         );
   }
 
-  pulsa(body, file) 
-  {
-      
-    let type = "application/json; charset=UTF-8";
-    let headers = new Headers({ 'Content-Type': type});
-    let options = new RequestOptions({ headers: headers });
-    
-    return this.http.post(this.serverpulsa + file, JSON.stringify(body), options)
-      .map(res => res.json());
-  }
-
-  sosmed(body, file) 
-  {
-        
-    let type = "application/json; charset=UTF-8";
-    let headers = new Headers({ 'Content-Type': type});
-    let options = new RequestOptions({ headers: headers });
-    return this.http.post(this.serversosmed + file, JSON.stringify(body), options)
-      .map(res => res.json());
-  }
-
-  traccar(body, file) 
-  {
-        
-    let type = "application/json; charset=UTF-8";
-    let headers = new Headers({ 'Content-Type': type});
-    let options = new RequestOptions({ headers: headers });
-    return this.http.post(this.servertracar + file, JSON.stringify(body), options)
-      .map(res => res.json());
-  }
+  
 
 }
